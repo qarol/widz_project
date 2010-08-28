3b68804688f1767f27529b2c7f71cd917a30a992
Karol Bajko <karol.bajko@gmail.com>
2010-08-28 14:27:18 +0200

    added validation to parent-child relation

--- app/models/parent_child.rb
+++ app/models/parent_child.rb
@@ -1,4 +1,10 @@
 class ParentChild < ActiveRecord::Base
   belongs_to :child, :class_name => "User"
   belongs_to :parent, :class_name => "User"
+
+  validate :parent_and_child_have_correct_roles
+
+  def parent_and_child_have_correct_roles
+    errors.add_to_base("Opiekun i uczeń muszą należeć do odpowiednich grup, by móc stworzyć relację") unless self.child.is_student? && self.parent.is_parent?
+  end
 end
