5795742b0cf575fe74a1b73f0007b67b766547ab
Karol Bajko <karol.bajko@gmail.com>
2010-09-13 03:48:13 +0200

    fix bug in attendances

--- app/controllers/teacher/lectures_controller.rb
+++ app/controllers/teacher/lectures_controller.rb
@@ -8,26 +8,26 @@ class Teacher::LecturesController < Teacher::TeacherController
   def show
     @lecture = Lecture.find(params[:id])
     @date = Date.strptime(params[:date], "%F")
-    @lecture_unit = LectureUnit.find_or_create_by_date(:date => @date, :lecture_id => @lecture.id)
+    @lecture_unit = LectureUnit.find_or_create_by_date_and_lecture_id(:date => @date, :lecture_id => @lecture.id)
   end
 
   def attendances
     @lecture = Lecture.find(params[:id])
     @date = Date.strptime(params[:date], "%F")
-    @lecture_unit = LectureUnit.find_or_create_by_date(:date => @date, :lecture_id => @lecture.id)
+    @lecture_unit = LectureUnit.find_or_create_by_date_and_lecture_id(:date => @date, :lecture_id => @lecture.id)
     @users = @lecture.subject.team.users.sort_by{|s| s.lastname}
   end
 
   def edit
     @lecture = Lecture.find(params[:id])
     @date = Date.strptime(params[:date], "%F")
-    @lecture_unit = LectureUnit.find_or_create_by_date(:date => @date, :lecture_id => @lecture.id)
+    @lecture_unit = LectureUnit.find_or_create_by_date_and_lecture_id(:date => @date, :lecture_id => @lecture.id)
   end
 
   def update
     @lecture = Lecture.find(params[:id])
     @date = Date.strptime(params[:date], "%F")
-    @lecture_unit = LectureUnit.find_or_create_by_date(:date => @date, :lecture_id => @lecture.id)
+    @lecture_unit = LectureUnit.find_or_create_by_date_and_lecture_id(:date => @date, :lecture_id => @lecture.id)
     if @lecture_unit.update_attributes(params[:lecture_unit])
       flash[:notice] = 'Lekcja została zaktualizowana'
       redirect_to teacher_lecture_path(@lecture, :date => @date)
@@ -39,7 +39,7 @@ class Teacher::LecturesController < Teacher::TeacherController
   def update_attendances
     @lecture = Lecture.find(params[:id])
     @date = Date.strptime(params[:day], "%F")
-    @lecture_unit = LectureUnit.find_or_create_by_date(:date => @date, :lecture_id => @lecture.id)
+    @lecture_unit = LectureUnit.find_or_create_by_date_and_lecture_id(:date => @date, :lecture_id => @lecture.id)
     @lecture_unit.attendances = User.find_all_by_id(params[:lecture_unit][:attendance_ids]).map{|u| Attendance.find_or_create_by_lecture_unit_id_and_user_id(:user_id => u.id, :lecture_unit_id => @lecture_unit.id)}
     if @lecture_unit.save
       flash[:notice] = "Zapisano frekwencję"
